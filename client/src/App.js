97c35fb1c94e5d4f9940fc9ece433c39783223fd
Hamid Hamidbeyli <hemhd.hd@gmail.com>
2022-10-13 22:46:38 +0300

    Theme Provide

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,92 @@
-import styled from "styled-components";
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from "./utils/Theme";
 
 const Container = styled.div`
   display: flex;
 `;
 const Main = styled.div`
   flex: 5;
-  background:yellow ;
-
+  background: ${({theme})=>theme.bg};
 `;
 const Wrapper = styled.div``;
 
-
 function App() {
-  return (
-    <Container>
-      <Menu />
+  const [darkMode, setDarkMode] = useState(false);
 
-      <Main>
-        <Navbar />
-        <Wrapper>maın wrapper</Wrapper>
-      </Main>
-    </Container>
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <Menu setDarkMode={setDarkMode} darkMode={darkMode} />
+        <Main>
+          <Navbar />
+          <Wrapper>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk</h1>
+            <h1>sdfcsdjndsk </h1>
+          </Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
