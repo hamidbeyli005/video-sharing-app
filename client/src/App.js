329911fdfb99d2af57419daef11c53d8b57bbe7a
Hamid Hamidbeyli <hemhd.hd@gmail.com>
2022-10-18 22:50:28 +0300

    video page
    video page and comments section

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,4 @@
-import { useState } from "react";
-import styled, { ThemeProvider } from "styled-components";
-import Menu from "./components/Menu";
-import Navbar from "./components/Navbar";
-import { darkTheme, lightTheme } from "./utils/Theme";
-import { Outlet } from "react-router-dom";
+
 import MainLayout from "./components/MainLayout";
 
 function App() {
--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -1,9 +1,166 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import {
+  BsHandThumbsUp,
+  BsHandThumbsDown,
+  BsSave,
+  BsFillReplyFill,
+} from "react-icons/bs";
+import Comments from "../components/Comments";
+
+const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+
+const Content = styled.div`
+  flex: 5;
+`;
+const VideoWrapper = styled.div``;
+
+const Title = styled.h1`
+  font-size: 18px;
+  font-weight: 400;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Info = styled.span`
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  gap: 20px;
+  color: ${({ theme }) => theme.text};
+`;
+
+const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+
+const Recommendation = styled.div`
+  flex: 2;
+`;
+const Channel = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ChannelInfo = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+const ChannelDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${({ theme }) => theme.text};
+`;
+
+const ChannelName = styled.span`
+  font-weight: 500;
+`;
+
+const ChannelCounter = styled.span`
+  margin-top: 5px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.textSoft};
+  font-size: 12px;
+`;
+
+const Description = styled.p`
+  font-size: 14px;
+`;
+
+const Subscribe = styled.button`
+  background-color: #cc1a00;
+  font-weight: 500;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  height: max-content;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
 
 const Video = () => {
   return (
-    <div>Video</div>
-  )
-}
+    <Container>
+      <Content>
+        <VideoWrapper>
+          <iframe
+            width="100%"
+            height="480"
+            src="https://www.youtube.com/embed/mkggXE5e2yk"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </VideoWrapper>
+        <Title>Test Video</Title>
+        <Details>
+          <Info>4,435,891 views • Oct 18, 2022</Info>
+          <Buttons>
+            <Button>
+              <BsHandThumbsUp /> like
+            </Button>
+            <Button>
+              <BsHandThumbsDown /> Dislike
+            </Button>
+            <Button>
+              <BsFillReplyFill /> Share
+            </Button>
+            <Button>
+              <BsSave /> Save
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOjB8EhUizZ_xiovY98D9Hwr_FLL-836V5wQ&usqp=CAU" />
+            <ChannelDetail>
+              <ChannelName>Hamid Hamidbayli</ChannelName>
+              <ChannelCounter>127K subscribers</ChannelCounter>
+              <Description>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Doloribus laborum delectus unde quaerat dolore culpa sit aliquam
+                at. Vitae facere ipsum totam ratione exercitationem. Suscipit
+                animi accusantium.
+              </Description>
+            </ChannelDetail>
+          </ChannelInfo>
+          <Subscribe>SUBSCRIBE</Subscribe>
+        </Channel>
+        <Hr />
+        <Comments />
+      </Content>
+      <Recommendation>dfıuv</Recommendation>
+    </Container>
+  );
+};
 
-export default Video
+export default Video;
